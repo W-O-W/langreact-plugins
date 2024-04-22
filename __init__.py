756e71d0944d1c1d6feb4e03bd0f7c78e8ef7c4f
zhangli.max <zhangli.max@bigo.sg>
2024-04-22 21:11:26 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+/*
+ * File Created: Monday, 22nd April 2024 9:10:25 pm
+ * Author: zhangli.max (zhangli.max@bigo.com)
+ * -----
+ * Last Modified: Monday, 22nd April 2024 9:10:25 pm
+ * Modified By: zhangli.max (zhangli.max@bigo.com)
+ */
